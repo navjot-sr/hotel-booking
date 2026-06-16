af375ff48de34e1b7666963b75e437bf6b8d8cb2
navjot-sr <srajpalnavjot3@gmail.com>
2026-06-16 19:03:24 +0530

    Auto save 2026-06-16 19:03:24

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div>
-            Hero
+        <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")]bg-no-repeat bg-cover bg-center h-screen'>
+
         </div>
     )
 }
