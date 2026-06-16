f1f79ca1d24460df2faeb55179ee93eb2c829d23
navjot-sr <srajpalnavjot3@gmail.com>
2026-06-16 19:13:28 +0530

    Auto save 2026-06-16 19:13:28

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Hero = () => {
     return (
         <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
-
+            <p className='bg-#FBBF24/50 rounded-[30px] px-4 py-2'>The Ultimate Hotel Experience</p>
+            <h1>Discover Your Perfect Gateway Destination</h1>
+            <p>Unparalleled luxury and comfort await at the world's most exclusive hotels and resorts. Start your journey today.</p>
         </div>
     )
 }
