91c58e021db3e1dad295b2a93cc11e7cc54bd159
navjot-sr <srajpalnavjot3@gmail.com>
2026-06-16 18:33:10 +0530

    Auto save 2026-06-16 18:33:10

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner")
   return (
     <div>
-      <Navbar />
+      {!isOwnerPath && <Navbar />}
     </div>
   )
 }
