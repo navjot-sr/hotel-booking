6a70861eefc38865f458a265a4f142a4bcaf269b
navjot-sr <srajpalnavjot3@gmail.com>
2026-06-17 16:18:22 +0530

    Auto save 2026-06-17 16:18:22

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { ClerkProvider } from '@clerk/react'
+
+//import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
