810eba738db3d56741eec866ac6bdbf7828dc000
navjot-sr <srajpalnavjot3@gmail.com>
2026-06-17 19:14:31 +0530

    Auto save 2026-06-17 19:14:31

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
 import { ClerkProvider } from '@clerk/react'
+import App from './App.jsx'
+import './index.css'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key")
+}
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" afterSignInUrl="/">
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignInUrl="/"
+      afterSignOutUrl="/"
+    >
       <App />
     </ClerkProvider>
   </BrowserRouter>
