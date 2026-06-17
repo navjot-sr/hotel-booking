86ef8d0030cfeba1b86f361352ac25e6d393bcfb
navjot-sr <srajpalnavjot3@gmail.com>
2026-06-17 16:23:24 +0530

    Auto save 2026-06-17 16:23:24

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
 
 //import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -13,6 +14,8 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" afterSignInUrl="/">
+      <App />
+    </ClerkProvider>
+  </BrowserRouter>
 )
